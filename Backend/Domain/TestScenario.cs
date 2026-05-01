003c64c57eb3d59a75cb63cdca378411f29dab71
Momad1n <Liliuson@mail.ru>
2026-05-01 21:44:14 +0300

    Patched files for CI

--- Backend/Domain/TestScenario.cs
+++ Backend/Domain/TestScenario.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Backend.Domain;
@@ -7,19 +9,18 @@ public class TestScenario
     public int Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
+
     public string RobotFile { get; set; } = string.Empty;
+
     public string Description { get; set; } = string.Empty;
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-    public int TestApplicationId { get; set; };
-
+    public int TestApplicationId { get; set; }
     [JsonIgnore]
     public TestApplication TestApplication { get; set; } = null!;
 
     [JsonIgnore]
-    public List<TestConfiguration> TestConfigurations { get; set; } = new ();
-
-    [JsonIgnore]
-    public List<TestRun> TestRuns { get; set; } = new ();
+    public List<TestConfiguration> TestConfigurations { get; set; } = new(); [JsonIgnore]
+    public List<TestRun> TestRuns { get; set; } = new();
 }
